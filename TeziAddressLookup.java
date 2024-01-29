be38f30c82742b446a1282b4c2d8a3005b954656
Mohit <mohit.patel1966@gmail.com>
2024-01-29 12:10:24 +0530

    print host location of tezi site

--- TeziAddressLookup.java
+++ TeziAddressLookup.java
@@ -1,9 +1,14 @@
 import java.net.*;
 
-public class Mohit{
+public class TeziAddressLookup{
     public static void main(String args[]){
         try{
             InetAddress in=InetAddress.getLocalHost();
+            InetAddress[] tezi=InetAddress.getAllByName("www.teziapp.com");
+
+            for(int i=0; i<tezi.length;i++){
+                System.out.println(tezi[i]);
+            }
             System.out.println(in);
         }catch(Exception e){
             System.out.println(e);
